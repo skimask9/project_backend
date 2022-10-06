c820c9290f70c20d68998e2a2edd1ce05e021d5a
skimask9 <pashapasha232@gmail.com>
2022-10-07 00:53:44 +0300

    fix

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,1 +1,15 @@
 from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
